d6e92e985a8438fac6b96d75ef41c754f29b430e
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-03-05 20:26:09 -0600

    Fourth commit

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-let row = 1;
-
 const submitExpense = document.getElementById('submit');
 submitExpense.addEventListener("click", displayExpense);
 
 function displayExpense(){
-    let amount = document.getElementById('amount')
-    let date = document.getElementById('date')
-    let location = document.getElementById('location')
-    let description = document.getElementById('description')
+    const amount = document.getElementById('amount').value;
+    const date = document.getElementById('date').value;
+    const location = document.getElementById('location').value;
+    const description = document.getElementById('description').value;
 
     if(!amount || !date || !location || !description ){
       alert("Please complete your entry");
       return false; 
     };
 
+    const table = document.getElementById('costs');
     const tableRow = document.createElement('tr');
     
     const newAmount = document.createElement('td');
     newAmount.textContent = amount
-    tableRow.appendChild(newAmount);
+    inputAmount = table + tableRow.appendChild(newAmount);
+    
 
     const newDate = document.createElement('td');
     newDate.textContent = date
-    tableRow.appendChild(newDate);
+    inputDate = table + tableRow.appendChild(newDate);
+    
 
     const newLocation = document.createElement('td');
     newLocation.textContent = location
-    tableRow.appendChild(newLocation);
+    inputLocation = table + tableRow.appendChild(newLocation);
+    
 
     const newDescription = document.createElement('td');
     newDescription.textContent = description
-    tableRow.appendChild(newDescription);
-
+    inputDescription = table + tableRow.appendChild(newDescription);
     
-  }
+
+  
+    
+    };
 
 
 
